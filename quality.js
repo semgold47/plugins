c6393aebad248621d3b5c7d4d5e7bc3915501d54
semgold47 <50056217+semgold47@users.noreply.github.com>
2025-11-04 23:45:01 +0300

    Update quality.js

--- quality.js
+++ quality.js
@@ -218,11 +218,11 @@
     var rate = $(".full-start-new__rate-line", renderEl);
     if (!rate.length) return;
     rate.css("visibility", "hidden");
-    var el = $(".full-start__status", renderEl);
+    var el = $(".full-start-new__details", renderEl);
     if (el.length) el.text(text).removeClass("my-loading");
     else
       rate.append(
-        '<div class="full-start__status my-quality">' + text + "</div>"
+        '<div class="full-start-new__details my-quality">' + text + "</div>"
       );
     rate.css("visibility", "visible");
   }
